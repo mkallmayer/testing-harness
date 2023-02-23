41f7b6d1291919f83eb1d000ba4e345128e588d1
Melvin <mel.kallmayer@gmail.com>
2023-02-23 01:08:17 +0100

    simplified use of BaseCorpus

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,25 +1,43 @@
 package helpers
 
 import (
+	"fmt"
 	"os"
 	"strings"
+	"testing"
 )
 
 type ErrorBlacklist struct {
 	Blacklist []string
 }
 
-func BaseCorpus(files []string) ([][]byte, error) {
+func loadBaseCorpus(files []string) ([][]byte, error) {
 	corpus := make([][]byte, len(files))
 	for i, f := range files {
 		b, err := os.ReadFile(f)
 		if err != nil {
 			corpus[i] = b
 		} else {
-			corpus[i] = make([]byte, 1)
+			fmt.Printf("Error while reading corpus file %s: %v", f, err)
+			return corpus, err
 		}
 	}
 	return corpus, nil
+}
+
+func addToCorpus(f *testing.F, corpus [][]byte) {
+	for _, b := range corpus {
+		f.Add(b)
+	}
+}
+
+func BaseCorpus(f *testing.F, files []string) {
+	corpus, err := loadBaseCorpus(files)
+	if err != nil {
+		fmt.Printf("Proceeding with empty initial corpus.")
+		return
+	}
+	addToCorpus(f, corpus)
 }
 
 func (b ErrorBlacklist) IsSerious(err error) bool {
